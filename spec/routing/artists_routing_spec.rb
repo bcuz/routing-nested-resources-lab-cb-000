1ea2efb973a283ff219b10fc887df2c93badddfc
adam n <adam.nawroc@gmail.com>
2018-04-19 18:02:28 +0000

    automatically backed up by learn

--- spec/routing/artists_routing_spec.rb
+++ spec/routing/artists_routing_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "artists routing", type: :routing do
 
   it "routes to songs as a nested resource of artist" do
     expect(get: artist_songs_path(1)).to route_to("songs#index", artist_id: "1")
-  end
+  endgit status
 
   it "routes to a song as a nested resource of an artist" do
     expect(get: artist_song_path(1, 2)).to route_to("songs#show", artist_id: "1", id: "2")
